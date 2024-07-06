a2403882a9e2f9e53223845ceec7c05f85294c20
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-06 11:58:21 +0100

    Complete sumAll task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (Number.isInteger(min) && Number.isInteger(max)){
+        if (min >= 0 && max >= 0){
+            let total = 0;
+            if (min > max){
+                const temp = min
+                min = max;
+                max = temp;
+            }
+           
+            for (i = min; i <= max; i++){
+                total+=i;
+            }
+            return total;
+        }
+    }
+    return 'ERROR'
 };
 
 // Do not edit below this line
