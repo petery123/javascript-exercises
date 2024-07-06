938f58e6eca9e4a7b1ab5e62f4637876eb171f82
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-06 12:19:37 +0100

    Complete tempConversion task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  const cel = (5/9) * (far - 32);
+  return Math.round(cel * 10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  const far = (9/5 * cel) + 32;
+  return Math.round(far * 10)/10;
 };
 
 // Do not edit below this line
