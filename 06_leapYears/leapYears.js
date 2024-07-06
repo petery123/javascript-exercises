c74e008d905cc210a5d23d44346ab66c08d3d821
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-06 12:09:12 +0100

    Complete leapYears task

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 100 === 0){
+        return (year % 400 === 0)? true : false;
+    };
+    if (year % 4 === 0) return true;
+    
+    return false;
 };
 
 // Do not edit below this line
