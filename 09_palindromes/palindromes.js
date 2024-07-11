b326bb302bfa0977604dfb4e4f3549e8d36f376f
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-11 23:17:28 +0100

    Complete task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const store = string
+            .split("")
+            .filter((char) => (/[a-zA-Z0-9]/)
+            .test(char))
+            .map((char) => char.toLowerCase());
+    reversed = store.slice().reverse();
+    return reversed.join("") == store.join("");
 };
+
+console.log(palindromes('racecar'));
 
 // Do not edit below this line
 module.exports = palindromes;
