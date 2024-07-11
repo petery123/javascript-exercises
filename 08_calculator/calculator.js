3f07ffccbb744f9f99e90d564eadd2f451787114
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-11 22:24:14 +0100

    Complete task 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+  return numList.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+  return numList.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return (Math.pow(num1,num2));
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	total = 1;
+  for (i = 0; i < num; i++){
+    total *= (i+1);
+  }
+  return total;
 };
 
 // Do not edit below this line
