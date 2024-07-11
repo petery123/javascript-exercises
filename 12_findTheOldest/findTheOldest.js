52fa095debc1d8557307a9554b6137550a62ab5b
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-12 00:09:29 +0100

    Complete task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.sort((first, second) => {
+        let [firstBirth, firstDeath] = [first.yearOfBirth, first.yearOfDeath];
+        let [secondBirth, secondDeath] = [second.yearOfBirth, second.yearOfDeath];
+        if (firstDeath === undefined){
+            firstDeath = 2024;
+        }
+        if (secondDeath === undefined){
+            secondDeath = 2024;
+        }
+        return ((firstDeath - firstBirth) - (secondDeath - secondBirth));
+    })
+    return array[array.length-1];
 };
 
 // Do not edit below this line
