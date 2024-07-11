3ff42083cb034b7e08b331890a72f2c552cfde0e
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-11 23:35:28 +0100

    Complete task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let [head, tail] = [1, 1]
+    num = +num;
+    if (num < 0){
+        return ("OOPS");
+    }
+    if (num === 0){
+        return 0;
+    }
+    for (i = 2; i < num; i++){
+        let store = tail;
+        tail = store + head;
+        head = store;
+    }
+    return tail;
 
 };
 
